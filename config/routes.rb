983c37e803b69cb844109257a8b0e3b19274c17e
ayumi_arai <0araiayumi@gmail.com>
2019-09-17 12:29:32 +0900

    device導入。:index, :showのみ未ログイン時も実行可能。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :blogs
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+  root 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
